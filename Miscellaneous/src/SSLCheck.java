a0ea045a9977b77cdd7791e30d73adaeb1c639ec
Gowthamraj <yathirv4@gmail.com>
2022-02-04 12:01:19 +0530

    first commit

---
+++ Miscellaneous/src/SSLCheck.java
@@ -0,0 +1,31 @@
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class SSLCheck {
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.chrome.driver", "/Users/chromedriver.exe");
+
+		ChromeOptions options = new ChromeOptions();
+		
+//		Old School Methods :)
+//		c.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
+//		c.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+		
+		options.setAcceptInsecureCerts(true);
+
+		WebDriver driver = new ChromeDriver(options);
+
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+
+		//Launching the URL
+		driver.get("https://expired.badssl.com/");
+		System.out.println("The page title is : " +driver.getTitle());
+		driver.close();
+	}
+}
