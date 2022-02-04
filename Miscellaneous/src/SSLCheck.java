c61bdb6a24efb1e2c2850febaa0b73279f7e5480
Gowthamraj <yathirv4@gmail.com>
2022-02-04 16:36:07 +0530

    Initial commit

--- Miscellaneous/src/SSLCheck.java
+++ Miscellaneous/src/SSLCheck.java
@@ -1,5 +1,7 @@
 import java.time.Duration;
+import java.util.Arrays;
 
+import org.openqa.selenium.Proxy;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -8,24 +10,34 @@ public class SSLCheck {
 	public static void main(String[] args) {
 
 		System.setProperty("webdriver.chrome.driver", "/Users/chromedriver.exe");
-
+		
+//		Set Capabilities 
 		ChromeOptions options = new ChromeOptions();
 		
 //		Old School Methods :)
-//		c.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, true);
-//		c.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+/*		c.setCapaSbility(CapabilityType.ACCEPT_INSECURE_CERTS, true);
+		c.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);*/
 		
 		options.setAcceptInsecureCerts(true);
-
+		
+//		How to Set Proxy
+		Proxy proxy = new Proxy();
+		proxy.setHttpProxy("localhost:8080");
+		options.setCapability("proxy", proxy);
+		
+//		to block pop-ups
+		options.setExperimentalOption("excludeSwitches",
+				Arrays.asList("disable-popup-blocking"));
+		
+//		Initializing webdriver 
 		WebDriver driver = new ChromeDriver(options);
-
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 
 
-		//Launching the URL
+//		Launching the URL
 		driver.get("https://expired.badssl.com/");
 		System.out.println("The page title is : " +driver.getTitle());
-		driver.close();
+//		driver.close();
 	}
 }
